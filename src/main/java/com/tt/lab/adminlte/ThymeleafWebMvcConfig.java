7871acf7feaa141f465c5882271af090443d552a
Ahmadi Alfauzy Harahap <alfauzy@gmail.com>
2017-10-30 23:59:10 +0700

    Initial commit

---
+++ src/main/java/com/tt/lab/adminlte/ThymeleafWebMvcConfig.java
@@ -0,0 +1,47 @@
+package com.tt.lab.adminlte;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class ThymeleafWebMvcConfig extends WebMvcConfigurerAdapter {
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
+		return viewResolver;
+	}
+	
+	@Bean
+	public ITemplateEngine templateEngine() {
+		
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		
+		return templateEngine;
+		
+	}
+
+	@Bean
+	public ITemplateResolver templateResolver() {
+		
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setCharacterEncoding("UTF-8");		
+		templateResolver.setCacheable(false);
+		templateResolver.setPrefix("classpath:/templates/");
+		templateResolver.setSuffix(".html");
+		
+		return templateResolver;
+	}
+
+}
